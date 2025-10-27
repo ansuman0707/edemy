89bdd9cd042a4dda7b2d23de35d74c5c5bf99835
ansuman0707 <ansumanmohapatra0707@gmail.com>
2025-10-28 00:00:55 +0530

    client and server pushed

---
+++ server/controllers/courseController.js
@@ -0,0 +1,65 @@
+import Course from "../models/Course.js"; 
+
+// Get All Courses
+export const getAllCourse = async (req, res)=>{
+    try {
+        const courses = await Course.find({isPublished: true}).
+        select(['-courseContent', '-enrolledStudents']).populate
+        ({path: 'educator'})
+
+        res.json({ success: true, courses})
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
+
+// Get Course by Id
+export const getCourseId = async (req, res)=>{
+    const {id} = req.params
+
+    try {
+        const courseData = await Course.findById(id).populate({path: 'educator'})
+
+        // Remove lectureUrl if isPreviewFree is false
+        courseData.courseContent.forEach(chapter => {
+            chapter.chapterContent.forEach(lecture => {
+                if(!lecture.isPreviewFree){
+                    lecture.lectureUrl = "";
+                }
+            })
+        })
+
+        res.json({ success: true, courseData })
+    } catch (error) {
+         res.json({ success: false, message: error.message })
+    }
+}
+
+// Purchase Course
+export const purchaseCourse = async (req, res)=>{
+    try {
+        const { courseId } = req.body
+        const { origin } = req.headers
+        const userId = req.auth.userId
+        const userData = await User.findById(userId)
+        const courseData = await Course.findById(courseId)
+
+        if (!userData || !courseData) {
+            return res.json({ success: false, message: 'Data Not Found!' });
+        }
+
+        const purchaseData = {
+            courseId: courseData._id,
+            userId,
+            amount: (courseData.coursePrice - courseData.discount * courseData.coursePrice / 100).toFixed(2),
+        }
+
+        const newPurchase = await Purchase.create(purchaseData)
+
+        // Stripe Gateway Initialize
+        const stripeInstance = new Stripe(process.env.STRIPE_SECRET_KEY)
+
+    } catch (error) {
+        
+    }
+} 
