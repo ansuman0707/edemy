f5db0ed86fa6659ab686c63d8a3a52585971008b
ansuman0707 <ansumanmohapatra0707@gmail.com>
2025-11-17 01:00:14 +0530

    toast lib imported

--- client/src/pages/educator/StudentsEnrolled.jsx
+++ client/src/pages/educator/StudentsEnrolled.jsx
@@ -3,6 +3,7 @@ import { dummyStudentEnrolled } from '../../assets/assets';
 import Loading from '../../components/student/Loading';
 import { AppContext } from '../../context/AppContext';
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const StudentsEnrolled = () => {
 
